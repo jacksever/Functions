72daac703068460d28ec5df3d1268c7031afb67b
Alexander Gorodnikov <aleks_gor2000@mail.ru>
2020-12-12 14:58:38 +0300

    functions: part 2

--- Function/Program.cs
+++ Function/Program.cs
@@ -8,7 +8,7 @@ namespace Function
 	{
 		static void Main(string[] args)
 		{
-			Task_3();
+			Task_8();
 		}
 
 		static void Task_1()
@@ -30,7 +30,7 @@ namespace Function
 			Func<string, int> min = text =>
 			{
 				List<int> parser = new List<int>();
-				foreach(var item in text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray()) 
+				foreach (var item in text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray())
 				{
 					parser.Add(int.Parse(item));
 				}
@@ -42,7 +42,7 @@ namespace Function
 
 		static void Task_4()
 		{
-			Func<string, string, bool> data = (text, action)  =>
+			Func<string, string, bool> data = (text, action) =>
 			{
 				string[] num = text.Split(new char[] { ' ' });
 
@@ -69,6 +69,139 @@ namespace Function
 			};
 
 			data(Console.ReadLine(), Console.ReadLine());
+		}
+
+		// PART 2
+
+		static void Task_5()
+		{
+			Func<string, bool> operations = data =>
+			{
+				var parser = new List<int>();
+				foreach (var item in data.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray())
+				{
+					parser.Add(int.Parse(item));
+				}
+
+				int[] result = parser.ToArray();
+
+				while (true)
+				{
+					var command = Console.ReadLine();
+
+					if (command == "end")
+						return true;
+
+					switch (command)
+					{
+						case "add":
+							for (int i = 0; i < result.Length; i++)
+								result[i]++;
+							break;
+
+						case "multiply":
+							for (int i = 0; i < result.Length; i++)
+								result[i] = result[i] * 2;
+							break;
+
+						case "subtract":
+							for (int i = 0; i < result.Length; i++)
+								result[i] = result[i] - 1;
+							break;
+
+						case "print":
+							for (int i = 0; i < result.Length; i++)
+								Console.Write(result[i] + " ");
+							break;
+					}
+				}
+			};
+
+			operations(Console.ReadLine());
+		}
+
+		static void Task_6()
+		{
+			Func<string, int, List<int>> reversList = (data, number) =>
+			{
+				var parser = new List<int>();
+				foreach (var item in data.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray())
+				{
+					parser.Add(int.Parse(item));
+				}
+
+				foreach (var item in parser.ToList())
+				{
+					if (item % number == 0)
+						parser.Remove(item);
+				}
+
+				parser.Reverse();
+				return parser;
+			};
+
+			foreach(var item in reversList(Console.ReadLine(), int.Parse(Console.ReadLine())))
+			{
+				Console.Write(item + " ");
+			}
+		}
+
+		static void Task_7()
+		{
+			Func<int, string, List<string>> checkNames = (number, data) =>
+			{
+				string[] text = data.Split(new char[] { ' ' });
+				var result = new List<string>();
+
+				foreach (var item in text)
+				{
+					if (item.Length <= number)
+						result.Add(item);
+				}
+
+				return result;
+			};
+
+			foreach (var item in checkNames(int.Parse(Console.ReadLine()), Console.ReadLine()))
+			{
+				Console.Write(item + " ");
+			}
+		}
+
+		static void Task_8()
+		{
+			Func<string, int[]> comparator = data =>
+			{
+				var parser = new List<int>();
+				foreach (var item in data.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray())
+				{
+					parser.Add(int.Parse(item));
+				}
+
+				var array = parser.ToArray();
+
+				Array.Sort(array, new NumberComparator());
+
+				return array;
+			};
+
+			foreach(var item in comparator(Console.ReadLine()))
+			{
+				Console.Write(item + " ");
+			}
+		}
+	}
+
+	public class NumberComparator : IComparer<int>
+	{
+		public int Compare(int x, int y)
+		{
+			if (y % 2 == 0)
+				return 1;
+			if (x % 2 == 0)
+				return -1;
+			else
+				return 0;
 		}
 	}
 }
